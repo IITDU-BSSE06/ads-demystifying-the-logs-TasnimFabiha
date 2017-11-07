80d3e1387bcfdcf7730955cdcb5fbeef9d2bc330
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 02:07:11 -0500

    error in length checking corrected

--- assignment1/reducer.py
+++ assignment1/reducer.py
@@ -6,7 +6,7 @@ import sys
 count = 0
 for line in sys.stdin:
     data = line.strip()
-    if len(data_mapped) != 1:
+    if len(data) != 1:
         # Something has gone wrong. Skip this line.
         continue
     if data == "10.99.99.186":
