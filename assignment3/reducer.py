bba3428cdecaff808fc3f70238b4f2bc27ab9c14
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 01:20:41 -0500

    Assignmet 3 reducer updated
    assignment 3 and 4  were done in same reducer previously

--- assignment3/reducer.py
+++ assignment3/reducer.py
@@ -8,10 +8,9 @@ thisCounter = 0
 maxPathCounter = -9999999
 # Loop around the data
 # It will be in the format key\tval
-# Where key is the store name, val is the sale amount
-#
-# All the sales for a particular store will be presented,
-# then the key will change and we'll be dealing with the next store
+# Where key is the file path, val is the file path hit amount
+
+
 
 for line in sys.stdin:
 	data_mapped = line.strip()
@@ -19,12 +18,9 @@ for line in sys.stdin:
     
 	if oldKey and oldKey != thisKey:
 		if maxPathCounter < thisCounter:
-			maxPathCounter = thisCounter
 			maxKey = oldKey
 		thisCounter = 0
 	oldKey = thisKey
 	thisCounter = thisCounter+1
-    
-#maxPath = max(maxPathCounter, thisCounter)
-print maxPathCounter
+
 print maxKey
