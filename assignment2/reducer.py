fb589eccd17ed8dfb7754cdca54582a92ab68579
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 03:16:12 -0500

    errors in lentgh checking solved

--- assignment2/reducer.py
+++ assignment2/reducer.py
@@ -6,7 +6,7 @@ import sys
 count = 0
 for line in sys.stdin:
     data = line.strip()
-    if len(data) != 1:
+    if not data:
         # Something has gone wrong. Skip this line.
         continue
     if data == "/assets/js/the-associates.js":
