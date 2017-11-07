12b257ed1ace802da4a7caf7fcca1210744f475b
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 02:50:02 -0500

    error in lenght checking solved

--- assignment5/reducer.py
+++ assignment5/reducer.py
@@ -4,8 +4,7 @@ import sys
 
 salesTotal = 0
 oldKey = None
-thisCounter = 0
-maxPathCounter = -9999999
+
 # Loop around the data
 # It will be in the format key\tval
 # Where key is the file path, val is the unique file count
@@ -14,7 +13,7 @@ uniquefile = 1
 
 for line in sys.stdin:
 	data_mapped = line.strip()
-	if len(data_mapped) != 1:
+	if not data_mapped :
         	# Something has gone wrong. Skip this line.
         	continue
 	thisKey = data_mapped
