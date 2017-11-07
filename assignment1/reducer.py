6830ebe7daf9aaadbb50a9f330600baeac316bef
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 02:04:10 -0500

    line spacing corrected

--- assignment1/reducer.py
+++ assignment1/reducer.py
@@ -7,10 +7,10 @@ count = 0
 for line in sys.stdin:
     data = line.strip()
     if len(data_mapped) != 1:
-        	# Something has gone wrong. Skip this line.
-            continue
+        # Something has gone wrong. Skip this line.
+        continue
     if data == "10.99.99.186":
-    	count = count+1
+        count = count+1
 
 print count
 
