0acaa3e45d859d76d69b543fb1eaca1bd1e9bc70
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-11-07 02:05:54 -0500

    length checking added

--- assignment2/reducer.py
+++ assignment2/reducer.py
@@ -6,6 +6,9 @@ import sys
 count = 0
 for line in sys.stdin:
     data = line.strip()
+    if len(data) != 1:
+        # Something has gone wrong. Skip this line.
+        continue
     if data == "/assets/js/the-associates.js":
     	count = count+1
 
